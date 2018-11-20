8b177d0f958c7780a53a01b5e122f6e5958d0a31
RiaBhat <riashriyabhat1998@gmail.com>
2018-11-20 22:57:24 +0530

    listView still not working

---
+++ app/src/main/java/com/example/android/codeshala/Competetive.java
@@ -0,0 +1,13 @@
+package com.example.android.codeshala;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Competetive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_competetive);
+    }
+}
--- app/src/main/java/com/example/android/codeshala/MainActivity.java
+++ app/src/main/java/com/example/android/codeshala/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -23,6 +28,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private TextView TV;
     private ListView list;
     private ArrayList<String> updates = new ArrayList<>();
+    public static final String TAG = "10";
 
     private DatabaseReference mDatabase;
     @Override
@@ -46,8 +53,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         //Database code
         mDatabase= FirebaseDatabase.getInstance().getReference().child("announcements");
+
+
            //mDatabase=mDatabase;
-        mDatabase.addChildEventListener(new ChildEventListener() {
+        /*mDatabase.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
@@ -75,6 +84,31 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });*/
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String value = dataSnapshot.getValue(String.class);
+
+                updates.add(value);
+
+                arrayAdapter.notifyDataSetChanged();
+
+                Toast.makeText(MainActivity.this,
+                        "list loading", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Value is: " + value);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //Toast.makeText(getApplicationContext(),"Some error occured", Toast.LENGTH_SHORT); );
+                Toast.makeText(MainActivity.this,
+                        "Some error occured", Toast.LENGTH_SHORT).show();
+                Log.w(TAG, "Failed to read value.");// error.toException());
             }
         });
 
@@ -107,6 +141,53 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.example, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.option1:
+            {
+                Intent i = new Intent(MainActivity.this,Mentors.class);
+                startActivity(i);
+                return true;
+            }
+            case R.id.option2:
+            {
+                Intent i = new Intent(MainActivity.this,Placements.class);
+                startActivity(i);
+                return true;
+            }
+            case R.id.option3:
+            {
+                Intent i = new Intent(MainActivity.this,Colleges.class);
+                startActivity(i);
+                return true;
+            }
+            case R.id.subitem1:
+            {
+                Intent i = new Intent(MainActivity.this,Competetive.class);
+                startActivity(i);
+                return true;
+            }
+            case R.id.subitem2:
+            {
+                Intent i = new Intent(MainActivity.this,Development.class);
+                startActivity(i);
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 }
